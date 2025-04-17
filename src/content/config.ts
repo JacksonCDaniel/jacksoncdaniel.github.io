18ecdefe73bc70011cbbdc4b2d1511366833f688
Jackson Daniel <jcdaniel+github@wisc.edu>
2025-04-17 00:00:24 +0000

    Add blogs

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        excerpt: z.string(),
+        image: z.string().optional()
+    })
+});
+
+export const collections = {
+    'blog': blogCollection,
+};
